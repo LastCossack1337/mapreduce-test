f3c10db3801b39ea0998c319039c5ef47ba13906
LastCossack1337 <s.mykhaylichenko@gmail.com>
2020-02-16 16:55:39 -0500

    updated

--- mapreduce-test-python/logstat2/reducer.py
+++ mapreduce-test-python/logstat2/reducer.py
@@ -15,20 +15,20 @@ for line in sys.stdin:
         pass
 
 #!/usr/bin/python
-from operator import itemgetter
-import sys
+# from operator import itemgetter
+# import sys
 
-dict_ip_count = {}
+# dict_ip_count = {}
 
-for line in sys.stdin:
-    line = line.strip()
-    ip, num = line.split('\t')
-    try:
-        num = int(num)
-        dict_ip_count[ip] = dict_ip_count.get(ip, 0) + num
+# for line in sys.stdin:
+#     line = line.strip()
+#     ip, num = line.split('\t')
+#     try:
+#         num = int(num)
+#         dict_ip_count[ip] = dict_ip_count.get(ip, 0) + num
 
-    except ValueError:
-        pass
+#     except ValueError:
+#         pass
 
 
 sorted_dict_ip_count = sorted(dict_ip_count.items(), key=itemgetter(0))
