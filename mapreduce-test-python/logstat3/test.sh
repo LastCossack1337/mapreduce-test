a4a19ec100ad977809b4614bbf124495ea2cd2d3
LastCossack1337 <s.mykhaylichenko@gmail.com>
2020-02-19 16:29:56 -0500

    Another try3

--- mapreduce-test-python/logstat3/test.sh
+++ mapreduce-test-python/logstat3/test.sh
@@ -7,8 +7,8 @@
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
 -file ../../mapreduce-test-python/logstat3/mapper.py -mapper ../../mapreduce-test-python/logstat3/mapper.py \
 -file ../../mapreduce-test-python/logstat3/reducer.py -reducer ../../mapreduce-test-python/logstat3/reducer.py \
--input /logstat3/input/* -output /logstat3/output/ > /c/Users/smykh/Desktop/testfolder/test.txt
-/usr/local/hadoop/bin/hdfs dfs -cat /logstat3/output/part-00000 > /c/Users/smykh/Desktop/testfolder/test2.txt
+-input /logstat3/input/* -output /logstat3/output/
+/usr/local/hadoop/bin/hdfs dfs -cat /logstat3/output/part-00000
 /usr/local/hadoop/bin/hdfs dfs -rm -r /logstat3/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /logstat3/output/
 ../../stop.sh
